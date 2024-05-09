d204b97cebc4684df10a75dc7be539dfb8526340
testoml <automation.testoml@gmail.com>
2024-05-09 14:45:13 -0400

    Add project files.

---
+++ Contact_API_E2E_Automation/UnitTest1.cs
@@ -0,0 +1,16 @@
+namespace Contact_API_E2E_Automation
+{
+    public class Tests
+    {
+        [SetUp]
+        public void Setup()
+        {
+        }
+
+        [Test]
+        public void Test1()
+        {
+            Assert.Pass();
+        }
+    }
+}
